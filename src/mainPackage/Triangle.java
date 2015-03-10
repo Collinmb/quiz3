aaac555ec06af6ef6b28a4f62326b93466f485ef
maste_000 <maste_000@128.4.126.139>
2015-03-10 02:20:26 -0400

    comment

---
+++ src/mainPackage/Triangle.java
@@ -0,0 +1,42 @@
+package mainPackage;
+import java.util.Scanner;
+public class Triangle {
+	double side1=1;
+	double side2=1;
+	double side3=1;
+	
+	public void defaultTrinagle(){
+		
+		System.out.print("enter three lengths for a triangle:");
+		double side1 = input.nextDouble();
+		double side2 = input.nextDouble();
+		double side3 = input.nextDouble();
+		
+		area = 1/2(side1*side2)sin(C);
+			return area
+	}
+	public double accessSide1(){
+		return side1;
+	}
+	public double accessSide2(){
+		return side2;
+	}
+	public double accessSide3(){
+		return side3;
+	}
+	public double getArea(double side1 side2 side3){
+		
+		System.out.print("enter three lengths for a triangle:");
+		double side1 = input.nextDouble();
+		double side2 = input.nextDouble();
+		double side3 = input.nextDouble();
+		
+		area = 1/2(side1*side2)sin(C);
+			return area
+	}
+	public double getPerimeter(double side1 side2 side3){
+		perimeter = side1+side2+side3
+				
+				return perimeter;
+	}
+}
